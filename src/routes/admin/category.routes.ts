14c0fd2cb14ae10f8eb475b05815f4d8af13b4ab
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2025-12-10 09:05:38 -0300

    feat: route and category routes

--- src/routes/admin/category.routes.ts
+++ src/routes/admin/category.routes.ts
@@ -1,11 +1,18 @@
 import { Router } from "express";
 import CategoryController from "../../controllers/category.controller";
+import { AdminMiddlewares } from "../../middlewares/admin.middleware";
 
 
 const router = Router();
 
-router.post('/add', CategoryController.create);
-router.delete('/delete/:id', CategoryController.create);
+router.post('/add',
+  AdminMiddlewares.checkCredentials,
+  CategoryController.create
+);
+router.delete('/delete/:id',
+  AdminMiddlewares.checkCredentials,
+  CategoryController.create
+);
 
 
 export const CategoryRouter = router;
--- src/routes/admin/project.routes.ts
+++ src/routes/admin/project.routes.ts
@@ -7,8 +7,8 @@ const router = Router();
 
 router.use('/categories', CategoryRouter);
 
-router.post("/add", ProjectController.create);
-router.patch("/update/:id", ProjectController.update);
+router.post('/add', ProjectController.create);
+router.patch('/update/:id', ProjectController.update);
 
 
 export const ProjectRouter = router;
