f9d90b78fdb6486f8283a01dc38808c4e56d9cd7
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2025-12-17 09:34:41 -0300

    feat: enable cors handling

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 
 import { adminRouter } from './core/routes/admin';
 import { ProjectRouter } from './core/routes/project.routes';
@@ -7,7 +8,10 @@ import { MessageRouter } from './core/routes/message.routes';
 
 const app = express();
 
+const { PORT, ALLOWED_DOMAIN } = process.env;
+
 app.use(express.json());
+app.use(cors({ origin: ALLOWED_DOMAIN }));
 
 app.use('/admin', adminRouter);
 app.use('/messages', MessageRouter);
@@ -19,9 +23,8 @@ app.get('/', (req, res) => {
   return res.json({ msg: '200 ok' });
 });
 
-const { PORT, DOMAIN } = process.env;
 
 app.listen(PORT ?? 8080, () => {
   console.log('API HAS STARTED!');
-  console.log(`LISTENING AT URL: ${DOMAIN} PORT ${PORT}`);
+  console.log(`LISTENING AT PORT: ${PORT}`);
 });
